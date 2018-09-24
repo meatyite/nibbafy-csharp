6532134d22c66f228cee4e82ba6a65d24ce9f5cc
sl4vkek <43294802+sl4vkek@users.noreply.github.com>
2018-09-24 14:06:06 +0300

    Add files via upload

---
+++ nibbify-csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace nibbifycsharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			Console.Write("Type something to nibbify: ");
+			string textToNibbify = Console.ReadLine();
+			string nibbifiedtext = textToNibbify.Replace('g', 'B');
+			nibbifiedtext = nibbifiedtext.Replace('G', 'B');
+			Console.WriteLine("Nibbafied text:\n" + nibbifiedtext);
+			Console.Write("Press Any key to Continue... ");
+			Console.ReadKey();
+		}
+    }
+}
